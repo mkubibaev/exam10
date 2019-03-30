f5df1bacf2071998e120aefc5de5f235d94165a9
mkubibaev <mkubibaev@gmail.com>
2019-03-30 12:06:58 +0600

    #1 Начальные настройки

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = 8000;
+
+const connection = mysql.createConnection({
+    'host'     : 'localhost',
+    'port'     : '3306',
+    'user'     : 'root',
+    'password' : '55558888',
+    'database' : 'exam10_murat_kubibaev',
+});
+
+app.use(express.json());
+app.use(cors());
+
+connection.connect(err => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port`);
+    });
+});
